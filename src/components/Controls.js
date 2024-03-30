674bf94e89fa6b4843ff03153d08c6f758eb05fc
Soo <parksk971031@gmail.com>
2024-03-30 14:51:12 -0400

    fixed controlled time too

--- src/components/Controls.js
+++ src/components/Controls.js
@@ -55,7 +55,7 @@ function Controls({
   useEffect(() => {
     const timeoutId = setTimeout(() => {
       setShowExitButton(true);
-    }, 5000);
+    }, 180000);
 
     return () => {
       clearTimeout(timeoutId);
